7c5c5d9c3376d49b1b625a956bd6c2883630c5b3
Stenzel <stenzell@ad.msoe.edu>
2019-10-08 19:00:16 -0500

    almost got the enemy to follow the player

--- Enemies/2.5D_Enemy.gd
+++ Enemies/2.5D_Enemy.gd
@@ -5,13 +5,14 @@ var gravity = Vector3.DOWN * 12
 var speed = 3
 var velocity = Vector3()
 var target = preload("res://Players/Character1.tscn").instance()
-onready var player_container = get_node("player_container")
+onready var body = get_node("Body")
 
 func _ready():
-		player_container.add_child(target)
+		body.add_child(target)
 
 func _physics_process(delta):
-	var velocity = Vector3()
 	velocity += gravity * delta
-	velocity += (target.get_global_transform().origin - get_global_transform().origin).normalized() * speed
-	velocity = move_and_slide(velocity, Vector3.UP)
+	velocity.x = 0
+	velocity.z = 0
+	velocity += (target.get_global_transform().origin - body.get_global_transform().origin).normalized() * speed
+	velocity = move_and_slide(velocity, Vector3.UP) #The "body." above shouldn't be there
