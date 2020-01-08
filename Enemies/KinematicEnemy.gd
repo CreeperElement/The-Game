4384f67854a97ae6c067deb7933b1e6052b722fc
Seth Fenske <fenskesd@gmail.com>
2020-01-07 19:10:51 -0600

    Fixed player movement script. Up/Down is y, right/left is Z. That's important. The player now springs around the target location.

--- Enemies/KinematicEnemy.gd
+++ Enemies/KinematicEnemy.gd
@@ -4,13 +4,9 @@ var gravity = Vector3.DOWN * 12
 #var max_speed = 8
 var speed = 4
 var terminal_velocity = 200
-var velocity = Vector3()
+var velocity = Vector3(0, 0, 0)
 var target = Vector3()
 
-onready var body = get_node("Body")
-#var t = body.get_transform()
-#var lastPos = t.origin()
-#var value = 0
 
 func _physics_process(delta):
 	velocity += gravity * delta
@@ -22,6 +18,6 @@ func _physics_process(delta):
 		var direction = target - transform.origin
 		direction = direction.normalized()
 		direction *= speed * delta
-		velocity.x = direction.x
-		velocity.y = direction.y
-	velocity = move_and_collide(velocity)
+		velocity.x += direction.x
+		velocity.z += direction.z
+	velocity = move_and_slide(velocity, Vector3.UP)
