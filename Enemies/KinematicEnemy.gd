e8dea23e6b902250a2d945b2ab1853422a07486f
Seth Fenske <fenskesd@gmail.com>
2020-01-21 18:20:26 -0600

    Moves back and forth

--- Enemies/KinematicEnemy.gd
+++ Enemies/KinematicEnemy.gd
@@ -1,23 +1,29 @@
 extends KinematicBody
 
 var gravity = Vector3.DOWN * 12
-#var max_speed = 8
-var speed = 4
+var acceleration = 4
+var max_speed = 8
 var terminal_velocity = 200
 var velocity = Vector3(0, 0, 0)
-var target = Vector3()
+var target = Vector3(0, 0, 0)
 
 
 func _physics_process(delta):
+	move(delta)
+
+func move(delta):
 	velocity += gravity * delta
 	if velocity.z > terminal_velocity:
 		velocity.z = terminal_velocity
 	var distance_to_target = transform.origin.distance_to(target)
 	
-	if distance_to_target > .001:
-		var direction = target - transform.origin
+	var direction = target - transform.origin
+	direction = Vector2(direction.x, direction.z)
+	if(direction.length() >= 1):
 		direction = direction.normalized()
-		direction *= speed * delta
-		velocity.x += direction.x
-		velocity.z += direction.z
+
+	direction *= acceleration * delta
+	velocity.x += direction.x
+	velocity.z += direction.y
+	
 	velocity = move_and_slide(velocity, Vector3.UP)
